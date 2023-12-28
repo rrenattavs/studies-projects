1cbf470b7f1a17056d934e47c440af8650f3b839
Renata Vieira <rrenattavs@gmail.com>
2023-12-28 12:53:38 -0700

    commit of user,main,files

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello World!"
+
+// const sales = "Toyota";
+
+// function carTypes(name) {
+//   return name === "Honda" ? name : `Sorry, we don't sell ${name}.`;
+// }
+
+// const car = { myCar: "Saturn", getCar: carTypes("Honda"), special: sales };
+
+// console.log(car.myCar); // Saturn
+// console.log(car.getCar); // Honda
+// console.log(car.special); // Toyota
+
+console.log("Hello")
---
+++ user.js
@@ -0,0 +1,7 @@
+let user = {
+    name: "Thomas",
+    age: 20,
+    address: "100 street SE, calgary"
+}
+
+console.log(user);
