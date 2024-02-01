8489ebd432ed1a84b01077bf012aea0c9d36b383
Renata Vieira <rrenattavs@gmail.com>
2024-02-01 14:56:45 -0700

    Finished the cat face

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,14 +1,19 @@
 // const myHeading = document.querySelector("h1");
 // myHeading.textContent = "Hello World!"
+
 // const sales = "Toyota";
+
 // function carTypes(name) {
 //   return name === "Honda" ? name : `Sorry, we don't sell ${name}.`;
 // }
+
 // const car = { myCar: "Saturn", getCar: carTypes("Honda"), special: sales };
+
 // console.log(car.myCar); // Saturn
 // console.log(car.getCar); // Honda
 // console.log(car.special); // Toyota
-// console.log("Hello");
-// var num = 10;
-// num = "30";
-// console.log(num);
+
+console.log("Hello");
+let num : number | string = 10;
+num = "30";
+console.log(num);
